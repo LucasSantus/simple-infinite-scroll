104662f36c62f17b27feea10ed2ff00149b58902
Lucas Santos <leos.developer@gmail.com>
2023-06-06 23:46:01 -0300

    :wastebasket: remove desnecessaries functions

--- src/utils/getDateRandomic.ts
+++
@@ -1,6 +0,0 @@
-export function getDateRandomic(): Date {
-  const startTime = new Date("2000-01-01").getTime();
-  const endTime = new Date("2020-12-31").getTime();
-  const randomTime = startTime + Math.random() * (endTime - startTime);
-  return new Date(randomTime);
-}
